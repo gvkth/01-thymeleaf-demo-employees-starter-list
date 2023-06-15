894ffa0244961dacd22492ad5a432ca590532df0
cuongvk <cuongvk.hcm@vnpt.vn>
2023-06-15 22:29:03 +0700

    Create=Thymeleaf+Controller+Model

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -34,6 +36,25 @@ public class EmployeeController {
 
 		return "list-employees";
 	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel){
+		//create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		theModel.addAttribute("employee",theEmployee);
+		return "employees/employee-form";
+	}
+
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+		//save the employee
+		employeeService.save(theEmployee);
+
+		//use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+	}
+
 }
 
 
